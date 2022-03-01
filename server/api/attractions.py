6b29e6d087fb597ab690854332bcfde9ac2f2f01
TWcamel <j0930495159@gmail.com>
2022-03-01 21:20:20 +0800

    no controllers and make em go to api

--- server/api/attractions.py
+++ server/api/attractions.py
@@ -10,3 +10,11 @@ def attraction(id):
     return render_template("attraction.html")
 
 
+@day_trip_attractions.route("/api/attractions/test")
+def test():
+    with db.DB() as _db:
+        sql_cmd = "SELECT 1 FROM dual"
+        res = _db.fetch_db(sql_cmd, is_fetch_one=False)
+        print(res)
+
+    return {"OK": True}
--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 from flask import *
 import configparser
-from controllers.home import day_trip_home
-from controllers.attractions import day_trip_attractions
-from controllers.booking import day_trip_booking
+from api.home import day_trip_home
+from api.attractions import day_trip_attractions
+from api.booking import day_trip_booking
 from models import attractions
 
 config = configparser.ConfigParser()
@@ -16,6 +16,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.register_blueprint(day_trip_home)
 app.register_blueprint(day_trip_attractions)
 app.register_blueprint(day_trip_booking)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=int(config['port']), host=config['host'])

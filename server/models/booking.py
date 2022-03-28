b75c7e3265d4f86f8ebc3a9bb6600b9047245443
TWcamel <j0930495159@gmail.com>
2022-03-29 01:43:14 +0800

    create booking table bug typo changed

--- server/models/booking.py
+++ server/models/booking.py
@@ -6,13 +6,14 @@ def create_booking_table():
     with db.DB() as _db:
         sql_cmd = """
             CREATE TABLE IF NOT EXISTS booking (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER NOT NULL,
-                attraction_id INTEGER NOT NULL,
-                date TEXT NOT NULL,
-                time TEXT NOT NULL,
-                FOREIGN KEY (user_id) REFERENCES user(id),
-                FOREIGN KEY (attraction_id) REFERENCES attraction(id)
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                USER_ID INTEGER NOT NULL,
+                CREATE_TIME TIMSTAMP DEFAULT CURRENT_TIMESTAMP,
+                PRINCE BIGINT NOT NULL,
+                ATTRACTION_ID INTEGER NOT NULL,
+                TYPE VARCHAR(255) NOT NULL,
+                FOREIGN KEY (USER_ID) REFERENCES USER(ID),
+                FOREIGN KEY (ATTRACTION_ID) REFERENCES ATTRACTION(ID)
             )
         """
         _db.crud(sql_cmd=sql_cmd)
